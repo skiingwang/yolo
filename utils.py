5f865e5cf99ea1c9c567ba2dde2f66d41a88a5e6
Yvan Wang <skiingwang@163.com>
2026-04-25 10:39:17 +0800

    增加模型保存与加载工具类。

--- utils.py
+++ utils.py
@@ -19,3 +19,42 @@ def read_json(path):
         return f'错误：{path}不存在!'
     except json.JSONDecodeError as e:
         return f'{path}文件解析错误！{e}'
+
+# 保存模型权重
+def save_weight(model, path):
+    import paddle
+    paddle.save(model.state_dict(), f'{path}.pdparams')
+
+# 加载模型权重
+def load_weight(model, path):
+    import paddle
+    state_dict = paddle.load(path)
+    model.set_dict(state_dict)
+
+# 计算交并比
+def calc_iou(pred_box, gt_box):
+    import paddle
+    # gt_box: [batch, s, s, 4（x, y, w, h）]
+    gt_x, gt_y = gt_box[..., 0], gt_box[..., 1]
+    gt_w, gt_h = gt_box[..., 2], gt_box[..., 3]
+    gt_x1, gt_y1 = gt_x - gt_w / 2, gt_y - gt_h / 2
+    gt_x2, gt_y2 = gt_x + gt_w / 2, gt_y + gt_h / 2
+
+    pred_x, pred_y = pred_box[..., 0], pred_box[..., 1]
+    pred_w, pred_h = pred_box[..., 2], pred_box[..., 3]
+    pred_x1, pred_y1 = pred_x - pred_w / 2, pred_y - pred_h / 2
+    pred_x2, pred_y2 = pred_x + pred_w / 2, pred_y + pred_h / 2
+
+    # 计算交集的左上角和右下角的x,y坐标
+    inter_x1, inter_y1 = paddle.maximum(gt_x1, pred_x1), paddle.maximum(gt_y1, pred_y1)
+    inter_x2, inter_y2 = paddle.minimum(gt_x2, pred_x2), paddle.minimum(gt_y2, pred_y2)
+
+    # 计算交集的面积
+    inter_w, inter_h = (inter_x2 - inter_x1).clamp(min=0), (inter_y2 - inter_y1).clamp(min=0)
+    inter_area = inter_w * inter_h
+
+    # 计算并集的面积
+    gt_area, pred_area = gt_w * gt_h, pred_w * pred_h
+    union_area = pred_area + gt_area - inter_area
+
+    return inter_area / (union_area + 1e-6)  # [batch, s, s]
